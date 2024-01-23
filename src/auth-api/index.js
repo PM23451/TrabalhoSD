5e5de7cdea239b4584e9d028a0125b8d75f3b9de
PM23451 <120373405+PM23451@users.noreply.github.com>
2024-01-23 22:29:58 +0000

    Commit

--- src/auth-api/index.js
+++ src/auth-api/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const knexConfig = require('./knexfile').db;
 const knex = require('knex')(knexConfig);
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', async (req, res) => {
     try {
         //Insert um user
@@ -40,17 +42,29 @@ app.post('/adduser', async (req, res) => {
 
 //Post que recebe o username e password e verifica se existe na tabela users
 app.post('/login', async (req, res) => {
+    //res.status(200).send('valoraleatorio');
     try {
         const { username, password } = req.body;
+        console.log(username);
+        console.log(password);
         const user = await knex('users').where({
             username,
             password
         }).select('username', 'password');
         if (user.length > 0) {
-            res.sendStatus(200);
+            // Criar token
+            // const token = jwt.sign({ username }, process.env.TOKEN_SECRET);
+            const token = "login feito com sucesso";
+
+            // Converter em JSON e enviar
+            const jsonToken = JSON.stringify(token);
+
+            res.status(200).send(jsonToken);
         } else {
+            // Se não houver usuário, retornar status 401 (Não autorizado)
             res.sendStatus(401);
         }
+
     } catch (error) {
         console.error(error);
         res.status(500).send('Error inserting data');
--- src/web/src/Menus/Login.js
+++ src/web/src/Menus/Login.js
@@ -1,31 +1,78 @@
-import React, {useEffect, useState} from "react";
-import {Box, CircularProgress, Container, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
-
-//Login
+import React, { useState } from "react";
+import {
+    Box,
+    CircularProgress,
+    Container,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+} from "@mui/material";
 
 function Login() {
-    //Consultar a API http://localhost:18080/login
-    const [login, setLogin] = useState(null);
-
     const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [token, setToken] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            //Debugguer
+
+            console.log("user"+username);
+            console.log("pass"+password);
+            // Fazer a requisição para a API de login
+            const response = await fetch("http://localhost:18080/login", {
+                method: "POST", // ou "GET", dependendo da sua API
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setToken(data);
+                // Lógica para tratamento de sucesso, por exemplo, salvar token no estado ou redirecionar para outra página
+            } else {
+                // Lógica para tratamento de erro
+                console.error("Erro ao fazer login");
+            }
+        } catch (error) {
+            console.error("Erro ao fazer login:", error);
+        }
+    };
 
     return (
-        <container>
+        <Container>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleLogin}>
                 <label>
                     Username:
-                    <input type="text" name="username" />
+                    <input
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </label>
                 <label>
                     Password:
-                    <input type="text" name="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
-        </container>
+            <h1>{token}</h1>
+            <h1>{username}</h1>
+            <h1>{password}</h1>
+        </Container>
     );
 }
 
 export default Login;
-
