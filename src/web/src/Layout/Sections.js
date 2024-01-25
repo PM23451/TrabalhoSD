5131e70ed56ab5b2ffcb81ffecf71314e6c37e04
13gui13 <guibar128@gmail.com>
2024-01-25 11:48:08 +0000

    commit crud/mudança lateral

--- src/web/src/Layout/Sections.js
+++ src/web/src/Layout/Sections.js
@@ -1,13 +1,14 @@
 import TopTeams from "../Menus/TopTeams";
 import Login from "../Menus/Login";
 import Registar from "../Menus/Registar";
+import CRUDE from "../Menus/CRUDE";
 
 const Sections = [
 
     {
-        id: "top-teams",
-        label: "Top Teams",
-        content: <TopTeams/>
+        id: "registar",
+        label: "Registar",
+        content: <Registar/>
     },
 
     {
@@ -17,15 +18,16 @@ const Sections = [
     },
 
     {
-        id: "registar",
-        label: "Registar",
-        content: <Registar/>
-    },/*
+        id: "top-teams",
+        label: "Top Teams",
+        content: <TopTeams/>
+    },
+
     {
         id: "crud",
         label: "CRUD Crashes",
         content: <CRUDE/>
-    },*/
+    },
 
     {
         id: "something-else",
